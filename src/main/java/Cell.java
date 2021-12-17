ae4379758401f8d860bf1badd2502a4826795b48
jonas <jonasgranbom@hotmail.com>
2021-12-17 12:46:35 +0100

    Changes by Jens

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -14,6 +14,37 @@ public class Cell {
         isAlive = alive;
     }
 
+    public int checkHowManyAliveNeighboursGivenRowAndCol(Cell[][] gameField, int row, int col){
+        int aliveNeighbours = 0;
+
+        if (row > 0 && col > 0 && gameField[row - 1][col - 1].isAlive()) {
+            aliveNeighbours++;
+        }
+        if (row > 0 && gameField[row - 1][col].isAlive()) {
+            aliveNeighbours++;
+        }
+        if (row > 0 && col < 9 && gameField[row - 1][col + 1].isAlive()) {
+            aliveNeighbours++;
+        }
+        if (col > 0 && gameField[row][col - 1].isAlive()) {
+            aliveNeighbours++;
+        }
+        if (col < 9 && gameField[row][col + 1].isAlive()) {
+            aliveNeighbours++;
+        }
+        if (row < 9 && col > 0 && gameField[row + 1][col - 1].isAlive()) {
+            aliveNeighbours++;
+        }
+        if (row < 9 && gameField[row + 1][col].isAlive()) {
+            aliveNeighbours++;
+        }
+        if (row < 9 && col < 9 && gameField[row + 1][col + 1].isAlive()) {
+            aliveNeighbours++;
+        }
+
+        return aliveNeighbours;
+    }
+
     @Override
     public String toString() {
         if (this.isAlive){
